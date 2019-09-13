49710c5f38c328890d0c0e964a5d73ddf5626f4e
alxfed <alxfed@gmail.com>
2019-09-13 18:57:12 -0500

    scrapy project

--- data/settings.py
+++ data/settings.py
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'data.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'data.pipelines.DataPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'data.pipelines.DataPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
