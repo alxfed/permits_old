045bb10cc4acac45a678bb3078905f7df858ddee
alxfed <alxfed@gmail.com>
2019-09-14 09:10:12 -0500

    list

--- data/spiders/list.py
+++ data/spiders/list.py
@@ -1,26 +1,60 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
 
 
 class ListSpider(scrapy.Spider):
     name = 'list'
+    start_urls = ['https://webapps1.chicago.gov/buildingrecords/home']
 
     # https://docs.scrapy.org/en/latest/topics/spiders.html?highlight=start_requests#spiders
-    def start_requests(self):
-        return [scrapy.FormRequest("http://www.example.com/login",
-                                   formdata={'user': 'john', 'pass': 'secret'},
-                                   callback=self.logged_in)]
-
-    def logged_in(self, response):
-        # here you would extract links to follow and return Requests for
-        # each of them, with another callback
-        pass
 
     def parse(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        return scrapy.FormRequest.from_response(
+            response,
+            formid='agreement',
+            formdata = {"agreement": "Y",
+                        "submit": "submit"},
+            clickdata={'submit': "submit"},
+            callback = self.after_agreement
+        )
+
+    def after_agreement(self, response):
+        print('ok')
+        pass
+
+"""
+<form id="agreement" class="city-search-form" action="agreement" method="post">
+<input type="hidden" name="_csrf" value="d8e9e354-f3c4-4f53-8403-0a9f17bd66d3">
+<div class="row city-row city-row-form">
+	<div class="input-xxs" style="width:30px;">
+		<input id="rbnAgreement1" name="agreement" type="radio" value="Y">		
+	</div>
+	<div class="input-lg">		
+		I accept the terms of this license 
+	</div>
+</div>	
+<div class="form-row city-row-form">	
+	<div class="input-xxs" style="width:30px;">
+		<input id="rbnAgreement2" name="agreement" type="radio" value="N" checked="checked">
+	</div>
+	<div class="input-xl">
+		I do
+		&nbsp;<b>not</b>&nbsp;
+		accept the terms of this license
+	</div>
+</div>
+<div class="row city-row city-row-form">
+	<div class="input-xxl">	
+	</div>
+</div>
+<div class="row city-row city-row-form">	
+	<div class="input-md">		
+		<button id="submit" type="submit" name="submit" class="btn btn-primary" style="margin-top: 0;">		
+			Submit
+		</button>	
+	</div>
+</div>		
+<div>
+<input type="hidden" name="_csrf" value="d8e9e354-f3c4-4f53-8403-0a9f17bd66d3">
+</div></form>
+"""
