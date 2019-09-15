3f4ad63ab2854df6c68287e46071c91dee73294d
alxfed <alxfed@gmail.com>
2019-09-15 08:20:02 -0500

    not found by search

--- data/spiders/list_a.py
+++ data/spiders/list_a.py
@@ -4,7 +4,6 @@ import sqlite3
 
 
 def agreement_failed(response):
-    # //*[@id="agreement.errors"]
     NO_AGREEMENT_XPATH = '//*[@id="agreement.errors"]/text()'
     no_agreement = response.xpath(NO_AGREEMENT_XPATH).get()
     if no_agreement.startswith('Please agree to the term of the site to confinue.'):
@@ -15,15 +14,21 @@ def agreement_failed(response):
 
 def not_found(response):
     # TODO: check if this is a 'not found' page
-    pass
+    # //*[@id="search"]/div[2]/p
+    NO_MATCH_XPATH = '//*[@id="search"]/div[2]/p/text()'
+    no_address_found = response.xpath(NO_MATCH_XPATH).get()
+    if no_address_found.startswith('The address match was not found.'):
+        return True
+    else:
+        return False
 
 class InspectionsListSpider(scrapy.Spider):
     name = 'insp_list_a'
     start_urls = ['https://webapps1.chicago.gov/buildingrecords/home']
-    # DB_PATH = '/media/alxfed/toca/dbase/fifthbase.sqlite'
-    # conn = sqlite3.connect(DB_PATH)  # , isolation_level=None) for working without commit
-    # curs = conn.cursor()
-
+    DB_PATH = '/media/alxfed/toca/dbase/fifthbase.sqlite'
+    conn = sqlite3.connect(DB_PATH)  # , isolation_level=None) for working without commit
+    conn.row_factory = sqlite3.Row
+    curs = conn.cursor()
 
     def parse(self, response):
         return scrapy.FormRequest.from_response(
@@ -39,54 +44,20 @@ class InspectionsListSpider(scrapy.Spider):
             self.logger.error("agreement failed!")
             return
         else:
-            item_list = 1, 2 # pd.read_csv('itemlist.csv', dtype=object)
-        for item in item_list:
+            self.curs.execute("SELECT Address From permits")
+            permits_list = self.curs.fetchall()
+        for permit in permits_list:
             yield scrapy.FormRequest.from_response(
                 response,
                 formid='search',
-                formdata = {"fullAddress": item['Address'],
+                formdata = {"fullAddress": permit['Address'],
                             "submit": "submit"},
-                callback = self.after_search
-                )
+                callback = self.after_search)
 
     def after_search(self, response):
-        print('ok')
-        pass
-
-
-"""
-<form id="agreement" class="city-search-form" action="agreement" method="post">
-<input type="hidden" name="_csrf" value="d8e9e354-f3c4-4f53-8403-0a9f17bd66d3">
-<div class="row city-row city-row-form">
-	<div class="input-xxs" style="width:30px;">
-		<input id="rbnAgreement1" name="agreement" type="radio" value="Y">		
-	</div>
-	<div class="input-lg">		
-		I accept the terms of this license 
-	</div>
-</div>	
-<div class="form-row city-row-form">	
-	<div class="input-xxs" style="width:30px;">
-		<input id="rbnAgreement2" name="agreement" type="radio" value="N" checked="checked">
-	</div>
-	<div class="input-xl">
-		I do
-		&nbsp;<b>not</b>&nbsp;
-		accept the terms of this license
-	</div>
-</div>
-<div class="row city-row city-row-form">
-	<div class="input-xxl">	
-	</div>
-</div>
-<div class="row city-row city-row-form">	
-	<div class="input-md">		
-		<button id="submit" type="submit" name="submit" class="btn btn-primary" style="margin-top: 0;">		
-			Submit
-		</button>	
-	</div>
-</div>		
-<div>
-<input type="hidden" name="_csrf" value="d8e9e354-f3c4-4f53-8403-0a9f17bd66d3">
-</div></form>
-"""
+        if not_found(response):
+            yield None
+        else:
+            # TODO: process the result page
+            inspections_list = []
+            yield inspections_list
