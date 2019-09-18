38de3923e049cf13684a47af350b8941cf870009
alxfed <alxfed@gmail.com>
2019-09-18 09:14:21 -0500

    dates transform

--- dates_transform.py
+++ dates_transform.py
@@ -4,6 +4,7 @@ and format the dates into a unix timestamp like format.
 """
 import pandas as pd
 from numpy import nan
+from datetime import datetime, date, time
 
 
 def dateparse(x):
@@ -17,8 +18,12 @@ def main():
     input_file_path = '/home/alxfed/Downloads/Building_Permits.csv'
     output_file_path = '/home/alxfed/Downloads/Transformed_Building_Permits.csv'
 
+    start_date  = datetime(2018, 12, 8, 0, 0)
+    end_date    = datetime(2019, 9, 18, 0, 0)
+
     the_file = pd.read_csv(input_file_path,
-                        parse_dates=['APPLICATION_START_DATE','ISSUE_DATE', ],
+                        parse_dates=['APPLICATION_START_DATE',
+                                     'ISSUE_DATE'],
                         date_parser=dateparse,
                         dtype=object)
     the_file.to_csv(output_file_path, index=False)
