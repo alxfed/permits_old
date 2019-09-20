29c968e52cfa8af977a3d3d116e5d4df3f382739
alxfed <alxfed@gmail.com>
2019-09-20 11:22:47 -0500

    read jl into pandas and back

--- jl_inspections.py
+++ jl_inspections.py
@@ -16,20 +16,18 @@ def write_pd_to_jl(data, outfile):
 def main():
     # renovation_alteration_scraped.jl
     INP_FILE = '/home/alxfed/dbase/new_construction_scraped.jl'
-    OUT_FILE = '/home/alxfed/dbase/new_construction_procssd.jl'
+    OUT_FILE = '/home/alxfed/dbase/inspections.jl'
     #df = read_into_pd(INP_FILE)
     #res = write_pd_to_jl(df, OUT_FILE)
+    results = set()
     with jsonlines.open(INP_FILE, mode='r') as reader:
-        with jsonlines.open(OUT_FILE, mode='a') as writer:
-            for line in reader:
-                full_address = line['full_address']
-                input_address = line['input_address']
-                range_address = line['range_address']
-                insp_table = pd.DataFrame.from_records(line['insp_table'])
-                perm_table = pd.DataFrame.from_records(line['perm_table'])
-                # transform
-                writer.write(line)
-        writer.close()
+        for line in reader:
+            insp_table = line['insp_table']
+            for row in insp_table:
+                if row['type_desc'] in results:
+                    pass
+                else:
+                    results.add(row['type_desc'])
     reader.close()
     return
 
