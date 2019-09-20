25da4b479f80bab05fd34d5343499c94ab0c00d6
alxfed <alxfed@gmail.com>
2019-09-20 10:51:40 -0500

    read jl into pandas and back

--- jl_transform.py
+++ jl_transform.py
@@ -17,16 +17,16 @@ def main():
     # renovation_alteration_scraped.jl
     INP_FILE = '/home/alxfed/dbase/new_construction_scraped.jl'
     OUT_FILE = '/home/alxfed/dbase/new_construction_procssd.jl'
-    df = read_into_pd(INP_FILE)
-    res = write_pd_to_jl(df, OUT_FILE)
+    #df = read_into_pd(INP_FILE)
+    #res = write_pd_to_jl(df, OUT_FILE)
     with jsonlines.open(INP_FILE, mode='r') as reader:
         with jsonlines.open(OUT_FILE, mode='a') as writer:
             for line in reader:
                 full_address = line['full_address']
                 input_address = line['input_address']
                 range_address = line['range_address']
-                insp_table = line['insp_table']
-                line['perm_table'][0] = {'amended': 246}
+                insp_table = pd.DataFrame(line['insp_table'])
+                perm_table = pd.DataFrame(line['perm_table'])
                 # transform
                 writer.write(line)
         writer.close()
