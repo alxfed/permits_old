6f3660da364b4872443a95380e34e6fdafddc450
alxfed <alxfed@gmail.com>
2019-09-23 09:27:49 -0500

    sending out a list of sms messages

---
+++ clicksend_sms.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""Sending out a list of SMS messages
+"""
+from __future__ import print_function
+import clicksend_client
+from clicksend_client import SmsMessage
+from clicksend_client.rest import ApiException
+from os import environ
+
+
+# Configure HTTP basic authorization: BasicAuth
+configuration = clicksend_client.Configuration()
+configuration.username = environ['USERNAME']
+configuration.password = environ['API_KEY']
+
+
+def main():
+    # create an instance of the API class
+    api_instance = clicksend_client.SMSApi(clicksend_client.ApiClient(configuration))
+    sms_message = SmsMessage(source="+13129709819",
+                             body="This is the body of the message.",
+                             to="+13129709819")
+    sms_messages = clicksend_client.SmsMessageCollection(messages=[sms_message])
+    try:
+        # Send sms message(s)
+        api_response = api_instance.sms_send_post(sms_messages)
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling SMSApi->sms_send_post: %s\n" % e)
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
+
+'''
+Body parameter
+
+{
+  "messages": [
+    {
+      "to": "+61411111111",
+      "source": "sdk",
+      "body": "body"
+    },
+    {
+      "list_id": 0,
+      "source": "sdk",
+      "body": "body"
+    }
+  ]
+}
+'''
