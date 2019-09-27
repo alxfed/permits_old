bdd8b9fd2e9a6ac75723dceff2f2f6cefb66b998
alxfed <alxfed@gmail.com>
2019-09-27 13:13:08 -0500

    phone verification

--- clicks_auth.py
+++ clicks_auth.py
@@ -4,14 +4,19 @@
 import requests
 import base64
 from os import environ
+from requests.auth import HTTPBasicAuth
 
 
 def main():
+    url = 'https://rest.clicksend.com/v3/sms/inbound'
     username = environ['USER_NAME'] # 'Aladdin'
     password = environ['API_KEY'] # 'open sesame'
     auth_str = username + ':' + password
     auth = base64.b64encode(auth_str.encode())
     # header Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==
+    head = 'Basic ' + auth.decode()
+    header = {'Authorization': head}
+    res = requests.get(url=url, auth=HTTPBasicAuth(username, password))
     print('ok')
     return
 
