b2784fb44d6e97b6293a4119d09616483e3d0b04
alxfed <alxfed@gmail.com>
2019-10-01 07:35:04 -0500

    permit applications

---
+++ download_permit_applications.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""https://webapps1.chicago.gov/buildingpermit/search/resetsearch.do
+application details: hhttps://webapps1.chicago.gov/buildingpermit/search/applicantinformation.do
+"""
+import requests
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
