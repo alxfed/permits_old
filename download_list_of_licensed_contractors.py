8eda75200dc256239e0d81f35d8eceb2bc626bce
alxfed <alxfed@gmail.com>
2019-10-01 08:03:58 -0500

    active licensed contractors

---
+++ download_list_of_licensed_contractors.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""https://www.chicago.gov/city/en/depts/bldgs/provdrs/gen_contract.html
+
+"""
+import requests
+
+
+def main():
+    # active General Contractors https://webapps1.chicago.gov/activegcWeb/
+    # etc.
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
