9cf1a6be411f85433c7103a771acfae7e26d24f2
alxfed <alxfed@gmail.com>
2019-10-23 18:47:31 -0500

    compare

--- compare_and_update_company.py
+++ compare_and_update_company.py
@@ -33,20 +33,17 @@ def main():
                 print('2. Company in the list:   ', license, '\n')
                 inputchar = input('y/n ? ')
                 if inputchar == 'y':
-                    print('this: ', license)
-        pass
-
-    companyId = '627118578'
-    parameters = {"properties":
-                  [
-                    {
-                      "name": "name",
-                      "value": "MBI realty"
+                    companyId = company['companyId']
+                    parameters = {"properties":
+                        [
+                            {
+                                "name": "name",
+                                "value": license
+                            }
+                        ]
                     }
-                  ]
-                 }
-    result = hubspot.companies.update_company(companyId, parameters)
-    print('ok')
+                    result = hubspot.companies.update_company(companyId, parameters)
+                    print('ok')
     return
 
 
