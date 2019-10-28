4511e233c87a1ea9d61f6d41e688d798a4138266
alxfed <alxfed@gmail.com>
2019-10-28 08:57:41 -0500

    line by line

--- compare_with_lists_of_licensed_and_existing.py
+++ compare_with_lists_of_licensed_and_existing.py
@@ -6,57 +6,46 @@ import numpy as np
 from numpy import nan
 
 
-def compare_with_state_and_licenses(row, present, reference):
+def compare_with_licenses_and_state(row, present, reference):
     company_name = ''
     address = ''
     phone = ''
     do_not_add = False
+    found_to_add = {}
+    unlicensed = {}
     general_contractor = row['general_contractor']
     one, sep, two = general_contractor.partition(' ')
     found = reference[reference['company_name'].str.find(sub=one) != -1]
     if found.empty:
-        print('\n\nFound no licenses for  ', general_contractor)
-        print('Adding it to the unlicensed DataFrame \n \n')
-        # unlicensed.append(row, ignore_index=True)
+        print('\n\nFound no licenses for  ', general_contractor, '\n\n')
+        unlicensed = {'company_name': general_contractor, 'address': '', 'phone': ''}
+        return found_to_add, unlicensed
     else:
         for index, refer in found.iterrows():
             company_name = refer['company_name']
             if company_name.startswith(general_contractor):
                 # which means that company_name has license and the address and phone are in the refer row
-                # time to check it against the companies in the system
-                # one_co, sep, two_co = company_name.partition(' ')
-                present['name'] = present['name'].str.upper()
-                exist = present[present['name'].str.find(sub=one) != -1]
-                if not exist.empty:
-                    # something found, let's make sure that it is it
-                    for ind, existing in exist.iterrows():
-                        existing_co_name = existing['name']
-                        if company_name.startswith(existing_co_name):
-                            print('Yeah, the licensed contractor ',
-                                  company_name, '  exists, and it is already in the system\n')
-                            do_not_add = True
-                        else:
-                            # print('The licensed contractor ', company_name,
-                            #      '  does not exist in the system yet')
-                            do_not_add = False
-                else:
-                    # not in the system right now, continue with it.
-                    do_not_add = False
-                if not do_not_add:
-                    company_name = company_name
-                    address = refer['address']
-                    phone = refer['phone']
-                else:
-                    address = ''
-                    phone = ''
+                found_to_add = {'company_name': company_name,
+                                'address': str(refer['address']).replace(u'\xa0', u' '),
+                                'phone': refer['phone']}
+                unlicensed = {}
+                break
             else:
-                # company_name = ''
-                # print('There is no exact match in licenses for ', general_contractor,
-                #      '  even though there are some  ', one, '  matches')
-                pass
-    return pd.Series([company_name, address, phone])
+                unlicensed = {'company_name': general_contractor, 'address': '', 'phone': ''}
+                return found_to_add, unlicensed
 
+    exist = present[present['name'].str.find(sub=one) != -1]
+    if not exist.empty:
+        # something found, let's make sure that it is it
+        for ind, existing in exist.iterrows():
+            existing_co_name = existing['name']
+            if company_name.startswith(existing_co_name):
+                print('Yeah, the licensed contractor ',
+                      company_name, '  exists, and it is already in the system\n')
+                found_to_add = {}
+                return found_to_add, unlicensed
 
+    return found_to_add, unlicensed
 
 
 def main():
@@ -67,24 +56,35 @@ def main():
     output_file_path                = '/home/alxfed/archive/new_licensed_contractors_for_ra_permits.csv'
     unlicensed_file_path            = '/home/alxfed/archive/unlicensed_contractors_in_ra_permits.csv'
 
-    unlicensed = pd.DataFrame()
-
     origin      = pd.read_csv(origin_file_path, dtype=object)
     input_perm = origin.drop_duplicates(subset=['general_contractor'], keep='first', inplace=False)
     present_st  = pd.read_csv(present_state_file_path, dtype=object)
+    present_st['name'] = present_st['name'].str.upper()
     licensed    = pd.read_csv(general_contractors_file_path, dtype=object)
 
     # prepare for the output
-    out = pd.DataFrame()
-    out[['company_name', 'address', 'phone']] = input_perm.apply(compare_with_state_and_licenses, axis=1,
-                                                                 present=present_st, reference=licensed)
-    # for index, permit_holder in input_perm.iterrows():
+    out = []
+    unlicen = []
 
-    #output
+    for index, permit_holder in input_perm.iterrows():
+        to_add, unlic = compare_with_licenses_and_state(permit_holder, present_st, licensed)
+        if to_add:
+            # to_a = pd.DataFrame(to_add)
+            out.append(to_add)
+        if unlic:
+            unlicen.append(unlic)
+
+    # output
+    # out[['company_name', 'address', 'phone']] = input_perm.apply(compare_with_state_and_licenses, axis=1,
+    #  present=present_st, reference=licensed)
+    out = pd.DataFrame(out)
     output = out[(out['company_name'] != '') & (out['address'] != '') & (out['address'] != np.nan)]
+    output = output.drop_duplicates(subset=['company_name'], keep='first', inplace=False)
     output.to_csv(output_file_path, index=False)
 
-    # unlicensed.to_csv(unlicensed_file_path, index=False)
+    unlicensed = pd.DataFrame(unlicen)
+    unlicensed = unlicensed.drop_duplicates(subset=['company_name'], keep='first', inplace=False)
+    unlicensed.to_csv(unlicensed_file_path, index=False)
     return
 
 
