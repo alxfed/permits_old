6a721ebea8eea712c2a0cc3461f3bcd6220b6ae4
alxfed <alxfed@gmail.com>
2019-11-03 06:05:35 -0600

    oauth

--- hubspot/oauth.py
+++ hubspot/oauth.py
@@ -2,7 +2,7 @@
 """...
 """
 import requests
-from . import constants
+from hubspot import constants
 from os import environ
 
 
@@ -14,7 +14,7 @@ def get_oauth_token():
     redir = environ['redirect_uri']
     code = environ['code']
     data = f'grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&redirect_uri={redir}&code={code}'
-    response = requests.request('POST', url=constants.OAUTH_TOKEN_URL, json=data, headers=headers)
+    response = requests.request('POST', url=constants.OAUTH_TOKEN_URL, data=data, headers=headers)
     if response.status_code == 400:
         print('400')
         return
@@ -22,8 +22,25 @@ def get_oauth_token():
 
 
 def refresh_oauth_token():
-    token = ''
-    return token
+    refresh_token_file = './refresh_token.txt'
+    authorization_token_file = './authorization_token.txt'
+    rtf = open(refresh_token_file, 'r')
+    refresh_token = rtf.read()
+    rtf.close()
+    headers = constants.oauth_header
+    client_id = environ['client_id']
+    client_secret = environ['client_secret']
+    data = f'grant_type=refresh_token&client_id={client_id}&client_secret={client_secret}&refresh_token={refresh_token}'
+    response = requests.request('POST', url=constants.OAUTH_TOKEN_URL, data=data, headers=headers)
+    res = response.json()
+    refresh_token = res['refresh_token']
+    rtf.write(refresh_token)
+    rtf.close()
+    authorization_token = res['access_token']
+    autf = open(authorization_token_file, 'w')
+    autf.write(authorization_token)
+    autf.close()
+    return authorization_token
 
 
 def main():
