6b19b6cc4609f05372a27eb2cec04aca9ed679d1
alxfed <alxfed@gmail.com>
2019-11-03 04:57:09 -0600

    oauth

--- hubspot/oauth.py
+++ hubspot/oauth.py
@@ -11,17 +11,23 @@ def get_oauth_token():
     headers = constants.oauth_header
     client_id = environ['client_id']
     client_secret = environ['client_secret']
-    redir = 'https://www.example.com/'
+    redir = environ['redirect_uri']
     code = environ['code']
     data = f'grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&redirect_uri={redir}&code={code}'
     response = requests.request('POST', url=constants.OAUTH_TOKEN_URL, json=data, headers=headers)
     if response.status_code == 400:
-        pass
-    return
+        print('400')
+        return
+    return response.json()
+
+
+def refresh_oauth_token():
+    token = ''
+    return token
 
 
 def main():
-
+    res = get_oauth_token()
     return
 
 
