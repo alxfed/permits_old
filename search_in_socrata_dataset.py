884348a7eb156ce2b359a0154613cf7d4181fb06
alxfed <alxfed@gmail.com>
2019-11-05 10:13:08 -0600

    removing the scaffolds

--- search_in_socrata_dataset.py
+++ search_in_socrata_dataset.py
@@ -39,43 +39,32 @@ def data_chunk(uri):
 def main():
     """Read a chunk with date_issued in predefined window
     """
+    unlicensed_gen_contractors_file = '/home/alxfed/archive/unlicensed_contractors_with_permits.csv'
+    contractors = pd.read_csv(unlicensed_gen_contractors_file, dtype=object)
+
     data = pd.DataFrame()
 
+    # dates if necessary
     start_dt = dt.datetime(year=2017, month=1, day=1, hour=0, minute=0, second=0)
     start_str = start_dt.strftime('%Y-%m-%dT%H:%M:%S')
     end_dt = dt.datetime(year=2019, month=11, day=4, hour=0, minute=0, second=0)
     end_str = end_dt.strftime('%Y-%m-%dT%H:%M:%S')
-    # license_status=AAI , expiration_date=
-    full_text_query_string='ONE STOP LOANS'
     # api_call = api_url + f'?$where=license_start_date between "{start_str}" and "{end_str}"'
-    api_call = api_url + '?license_status=AAI' + f'&$q={full_text_query_string}' #  + ' AND ' + ''
-    # https://data.cityofchicago.org/resource/tt4n-kn4t.json?$where=job_titles like '%25CHIEF%25'
-    # here %25 are URL encoded symbol '%' standing for any number of any characters
 
-    limit = 500  # limit of the frame within the time window
-    offset = 0
-    data_to_read_left = True
+    # license_status=AAI , expiration_date=
+    # api_call = api_url + '?license_status=AAI' + f'&$q={full_text_query_string}' #  + ' AND ' + ''
 
-    while data_to_read_left:
-        api_frame = api_call#  + f'&$limit={limit}&$offset={offset}'
-        new_chunk = pd.DataFrame.from_records(data_chunk(api_frame))
-        new_chunk['issue_date'] = pd.to_datetime(new_chunk['issue_date'])
-        new_chunk['application_start_date'] = pd.to_datetime(new_chunk['application_start_date'])
-        new_chunk['reported_cost'] = pd.to_numeric(new_chunk['reported_cost'], downcast='unsigned')
-        data = data.append(new_chunk, sort=False, ignore_index = True)
-        if new_chunk.id.count() == limit:
-            offset += limit
+    for index, contractor in contractors.iterrows():
+        like = contractor['name']
+        likeness, sep, rest = like.partition(' ')
+        api_call = api_url + f"?$where=legal_name like '%25{rest}%25'" # here %25 are URL encoded symbol '%' standing for any number of any characters
+        found = pd.DataFrame.from_records(data_chunk(api_call))
+        if found.empty:
+            print('Not found ', likeness)
         else:
-            data_to_read_left = False
-    big_permits = data[data['reported_cost'] > 100000]
-    new_construction = data[data['permit_type'] == 'PERMIT - NEW CONSTRUCTION']
-    large_newconst = big_permits[big_permits['permit_type'] == 'PERMIT - NEW CONSTRUCTION']
-    large_newconst.to_csv('/media/alxfed/toca/aa-crm/regular/downloaded/last_week_newconst.csv', index=False)
-    large_newconst.to_json('/media/alxfed/toca/aa-crm/regular/downloaded/last_week_newconst.jl', orient='records', lines=True)
-    renovation = data[data['permit_type'] == 'PERMIT - RENOVATION/ALTERATION']
-    large_renow = big_permits[big_permits['permit_type'] == 'PERMIT - RENOVATION/ALTERATION']
-    large_renow.to_csv('/media/alxfed/toca/aa-crm/regular/downloaded/last_week_renovation.csv', index=False)
-    large_renow.to_json('/media/alxfed/toca/aa-crm/regular/downloaded/last_week_renovation.jl', orient='records', lines=True)
+            data = data.append(found, sort=False, ignore_index=True)
+            print('Added found for ', likeness)
+    data.to_csv('/media/alxfed/archive/found_licenses.csv', index=False)
     return
 
 
