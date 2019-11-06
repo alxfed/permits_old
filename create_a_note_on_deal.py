a49eae9661306e544c19a6d3fa2d568a3fc20273
alxfed <alxfed@gmail.com>
2019-11-06 17:12:46 -0600

    removing the scaffolds

--- create_a_note_on_deal.py
+++ create_a_note_on_deal.py
@@ -7,15 +7,15 @@ import datetime as dt
 
 
 def main():
-    ownerId = '313468425'
-    dealId = '1143450728'
-    appId = '205476'
+    ownerId = 40202623 # Data Robot
+    dealId = 1143450728
+    appId = 205476
     note_date = dt.datetime(year=2019, month=10, day=18, hour=0, minute=0, second=0)
     hubspot_timestamp = int(note_date.timestamp()*1000)
-    note_text = 'this is the note text'
-    params = {'ownerId': appId, 'timestamp': hubspot_timestamp, 'dealId': dealId,
+    note_text = 'this is the note text</br> Etc.'
+    params = {'ownerId': ownerId, 'timestamp': hubspot_timestamp, 'dealId': dealId,
               'note': note_text}
-    res =
+    res = hubspot.engagements.create_engagement_note(params)
     return
 
 
--- create_inspection_notes_from_jl.py
+++ create_inspection_notes_from_jl.py
@@ -3,13 +3,15 @@
 """
 import jsonlines
 import pandas as pd
+import hubspot
+import datetime as dt
 
 
 def main():
     INP_FILE = '/home/alxfed/archive/deals_inspections.jl'
     OUT_FILE = '/home/alxfed/archive/inspections_procssd.jl'
     reference_file_path = '/home/alxfed/archive/deals_downloaded.csv'
-    ownerId = '313468425'
+    ownerId = 40202623  # Data Robot
 
     permit_inspections = ['PERMIT INSPECTION', 'BLDG_PERM IRON PERMIT INSP', 'VENT/HEAT PERMIT INSPECTION',
                           'WATER DEPT PERMIT INSPECTION', 'ELECTRICAL PERMIT INSPECTION', 'CONSTRUCTION EQUIPMENT PERMIT',
@@ -24,11 +26,22 @@ def main():
                 full_address = line['full_address']
                 input_address = line['input_address']
                 range_address = line['range_address']
-                insp_table = pd.DataFrame.from_records(line['insp_table'])
-                insp_table['insp_date'] = pd.to_datetime(insp_table['insp_date'], infer_datetime_format=True)
+                permit = line['permit']
                 perm_table = pd.DataFrame.from_records(line['perm_table'])
                 perm_table['perm_date'] = pd.to_datetime(perm_table['perm_date'], infer_datetime_format=True)
-                permit = perm_table.iloc[0]
+                permeat = perm_table.loc[perm_table['permit_n'] == permit]
+
+                dealId = 1143450728
+                note_date = dt.datetime(year=2019, month=10, day=18, hour=0, minute=0, second=0)
+                hubspot_timestamp = int(note_date.timestamp() * 1000)
+                note_text = permeat.to_string(index=False)
+                params = {'ownerId': ownerId, 'timestamp': hubspot_timestamp, 'dealId': dealId,
+                          'note': note_text}
+                res = hubspot.engagements.create_engagement_note(params)
+
+                insp_table = pd.DataFrame.from_records(line['insp_table'])
+                insp_table['insp_date'] = pd.to_datetime(insp_table['insp_date'], infer_datetime_format=True)
+                tab = insp_table.to_html(index=False)
                 # transform
                 writer.write(line)
         writer.close()
