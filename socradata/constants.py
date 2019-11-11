188dbbfe82803429f6e86fd7e0e7563d9fa71d22
alxfed <alxfed@gmail.com>
2019-11-10 19:40:13 -0600

    socrdata

--- socradata/constants.py
+++ socradata/constants.py
@@ -9,6 +9,14 @@ import requests as rq
 DISCOVERY_API_URL = 'http://api.us.socrata.com/api/catalog/v1'
 CHICAGO_RESOURCE_URL = 'data.cityofchicago.org'
 
+# socrata key authorization
+SOCRATA_API_TOKEN_FILE    = '/home/alxfed/credo/socrata_api_token.txt'
+token_file = open(SOCRATA_API_TOKEN_FILE, 'r')
+socrata_api_token = token_file.read()
+token_file.close()
+
+socrata_authorization_header = {'Content-Type': 'application/json', 'X-App-Token': socrata_api_token}
+
 def main():
     return
 
--- socradata/datasets.py
+++ socradata/datasets.py
@@ -2,6 +2,17 @@
 """...
 """
 from .constants import *
+import requests
+
+
+def where_between(dataset, parameter, first_value, second_value):
+    api_url = f'https://{CHICAGO_RESOURCE_URL}/resource/{dataset}.json'
+    api_uri = api_url + f'?$where={parameter} between "{first_value}" and "{second_value}"'
+    response = requests.request("GET", url=api_uri, headers=socrata_authorization_header)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return
 
 
 def main():
